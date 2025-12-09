902be369662269f24ca2f8cc9781525311a0e30f
Nilfred Baez <60485220+nilfredb@users.noreply.github.com>
2025-12-08 22:28:54 -0400

    Change database name in get_engine function

--- core/db_engine.py
+++ core/db_engine.py
@@ -8,10 +8,11 @@ load_dotenv()
 
 def get_engine():
     server = os.getenv("DB_SERVER", "localhost")
-    database = os.getenv("DB_NAME", "SistemaOpiniones")
+    database = os.getenv("DB_NAME", "DWopiniones")
     driver = os.getenv("DB_DRIVER", "ODBC Driver 17 for SQL Server")
     
     conn_str = f"DRIVER={driver};SERVER={server};DATABASE={database};Trusted_Connection=yes;TrustServerCertificate=yes"
     
     conn_url = "mssql+pyodbc:///?odbc_connect=%s" % urllib.parse.quote_plus(conn_str)
     return create_engine(conn_url, fast_executemany=True)
+
